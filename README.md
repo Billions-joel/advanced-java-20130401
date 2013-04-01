f8315a2b9f66ed9d7865f4bbef694307b7956a9c
Dan Rosen <dr@mergeconflict.com>
2013-04-01 10:45:51 -0700

    shapes outline

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Advanced Java - April 1-3, 2013
 ===============================
+
+Object Identity
+---------------
+
+* Reference Identity
+  - `a == b`
+* Structural Identity (or Value Identity)
+  - `a.equals(b)`
---
+++ shapes/src/training/shapes/ShapesTest.java
@@ -0,0 +1,39 @@
+package training.shapes;
+
+import static org.junit.Assert.*;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class ShapesTest {
+
+	private static final Random RANDOM = new Random();
+
+	/*
+	 * equal rectangles must be equal
+	 * equal rectangles must have equal hashcodes
+	 * rectangle: setWidth shouldn't affect height and vice-versa
+	 * squares must remain square
+	 */
+
+	@Test
+	public void equalRectanglesMustBeEqual() {
+		for (int i = 0; i < 100; i++) {
+			Rectangle
+				r1 = arbitraryRectangle(),
+				r2 = new Rectangle(r1.getWidth(), r1.getHeight());
+			assertEquals(r1, r2);
+		}
+	}
+
+	private static Rectangle arbitraryRectangle() {
+		return RANDOM.nextBoolean()
+			? new Rectangle(RANDOM.nextInt(50), RANDOM.nextInt(50))
+			: arbitrarySquare();
+	}
+
+	private static Square arbitrarySquare() {
+		return new Square(RANDOM.nextInt(50));
+	}
+}
