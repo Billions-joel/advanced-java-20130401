ed6a1f1ff39be52b6843139a2ca553c004a74f46
Dan Rosen <dr@mergeconflict.com>
2013-04-02 15:35:36 -0700

    BlockingQueue

--- concurrency/src/training/concurrency/Logger.java
+++ concurrency/src/training/concurrency/Logger.java
@@ -1,34 +1,28 @@
 package training.concurrency;
 
-import java.util.concurrent.atomic.AtomicReference;
-
-import training.generics.ImmutableQueue;
-import training.generics.ImmutableQueue.DequeueResult;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Logger implements Runnable {
 
-	private final AtomicReference<ImmutableQueue<String>> messagesRef =
-			new AtomicReference<ImmutableQueue<String>>(ImmutableQueue.<String>empty());
+	private final BlockingQueue<String> messages = new LinkedBlockingQueue<String>();
 
 	public void log(String message) {
-		ImmutableQueue<String> oldMessages, newMessages;
-		do {
-			oldMessages = messagesRef.get();
-			newMessages = oldMessages.enqueue(message);
-		} while (!messagesRef.compareAndSet(oldMessages, newMessages));
+		try {
+			messages.put(message);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 	@Override
 	public void run() {
-		while (true) {
-			ImmutableQueue<String> oldMessages = messagesRef.get();
-			if (!oldMessages.isEmpty()) {
-				DequeueResult<String> result = oldMessages.dequeue();
-				String message = result.head;
-				ImmutableQueue<String> newMessages = result.tail;
-				if (messagesRef.compareAndSet(oldMessages, newMessages))
-					System.out.println(message);
+		try {
+			while (true) {
+				System.out.println(messages.take());
 			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
 		}
 	}
 
