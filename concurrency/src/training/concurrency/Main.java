682a16f0ae0308a0d0c71fab43fe38dc856e8cac
Dan Rosen <dr@mergeconflict.com>
2013-04-02 10:51:19 -0700

    Thread.join()

--- concurrency/src/training/concurrency/Main.java
+++ concurrency/src/training/concurrency/Main.java
@@ -2,13 +2,19 @@ package training.concurrency;
 
 public class Main {
 
-	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			Thread t = new Thread(new Fibonacci());
-			t.start();
-		}
+	public static void main(String[] args) throws InterruptedException {
 		
-		// System.out.println("done");
+		Thread[] threads = new Thread[10];
+		for (int i = 0; i < 10; i++) {
+			threads[i] = new Thread(new Fibonacci());
+			threads[i].start();
+		}
+
+		for (Thread t : threads) {
+			t.join();
+		}
+
+		System.out.println("done");
 	}
 
 }
