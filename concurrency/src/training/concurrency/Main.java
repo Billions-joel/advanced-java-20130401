4eea5603800e6af1b7dede6e1394c46938e644a8
Dan Rosen <dr@mergeconflict.com>
2013-04-02 11:47:47 -0700

    ExecutorService

--- concurrency/src/training/concurrency/Main.java
+++ concurrency/src/training/concurrency/Main.java
@@ -1,18 +1,21 @@
 package training.concurrency;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
 		
-		Thread[] threads = new Thread[10];
-		for (int i = 0; i < 10; i++) {
-			threads[i] = new Thread(new Fibonacci());
-			threads[i].start();
+		ExecutorService pool = Executors.newFixedThreadPool(10);
+		for (int i = 0; i < 20; i++) {
+			pool.submit(new Fibonacci());
 		}
 
-		for (Thread t : threads) {
-			t.join();
-		}
+		pool.shutdown();
+		pool.awaitTermination(10, TimeUnit.SECONDS);
 
 		System.out.println("done");
 	}
