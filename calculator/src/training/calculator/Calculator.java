923aa9d048e80cdc3374aa43fea104e7991630ef
Dan Rosen <dr@mergeconflict.com>
2013-04-03 10:11:57 -0700

    calculator project outline

---
+++ calculator/src/training/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package training.calculator;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// 1. get first command line arg and tokenize it (split on whitespace)
+		// 2. for each token:
+		//    - if it's a number, push
+		//    - if it's an operator, pop operands, push result
+		// 3. get answer from stack
+	}
+
+}
