1348f89a2d0c6e733390f53c330f4de393b1f278
Dan Rosen <dr@mergeconflict.com>
2013-04-03 14:58:26 -0700

    client-server hello world

--- client-server/src/training/clientserver/Client.java
+++ client-server/src/training/clientserver/Client.java
@@ -1,11 +1,16 @@
 package training.clientserver;
 
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
 
 	public static void main(String[] args) throws Throwable {
-		// open socket connection to server
-		// get output stream and ... println somehow
-		// close connection
+		Socket server = new Socket(InetAddress.getLocalHost(), 31337);
+		PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+		out.println("hello, server");
+		server.close();
 	}
 
 }
--- client-server/src/training/clientserver/Server.java
+++ client-server/src/training/clientserver/Server.java
@@ -1,11 +1,19 @@
 package training.clientserver;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
 	public static void main(String[] args) throws Throwable {
-		// wait for client socket on some port
-		// get inputstream and ... readln somehow
-		// close everything
+		ServerSocket server = new ServerSocket(31337);
+		Socket client = server.accept();
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		System.out.println(in.readLine());
+		client.close();
+		server.close();
 	}
 
 }
