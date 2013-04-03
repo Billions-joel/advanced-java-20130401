6fb81066561e64d3905d06749577ba16e33281a3
Dan Rosen <dr@mergeconflict.com>
2013-04-03 14:42:29 -0700

    client-server outline

---
+++ client-server/src/training/clientserver/Client.java
@@ -0,0 +1,11 @@
+package training.clientserver;
+
+public class Client {
+
+	public static void main(String[] args) throws Throwable {
+		// open socket connection to server
+		// get output stream and ... println somehow
+		// close connection
+	}
+
+}
---
+++ client-server/src/training/clientserver/Server.java
@@ -0,0 +1,11 @@
+package training.clientserver;
+
+public class Server {
+
+	public static void main(String[] args) throws Throwable {
+		// wait for client socket on some port
+		// get inputstream and ... readln somehow
+		// close everything
+	}
+
+}
